4df7ff07be165c5bc8aca8e95747982db850c43e
Evandro <evandro.guimaraes@pmweb.com.br>
2020-06-04 11:06:24 -0300

    consolidated routes and early app for Back End

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,47 +1,17 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+
+import path from 'path';
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-//Rota: Endereço completo da requisição
-//Recurso: Qual entidade estamos acessando do sistema 
+app.use(routes);
 
-const users =[
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel'
-]
-
-app.get('/users',(request,response)=>{
-
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-    console.log('Listagem de usuários ');
-    response.json(filteredUsers);
-});
-
-app.get('/users/:id',(request,response)=>{
-    const id = Number(request.params.id);
-
-    const user = users[id];
-     
-    response.json(user);
-});
-
-app.post('/users', (request,response) => {
-    
-    const data = request.body;
-
-    const user = {
-
-        name: data.name,
-        email: data.email
-    };
-    
-    return response.json(user);
-});
+app.use('/uploads', express.static(path.resolve(__dirname,'..', 'uploads' )));
 
 app.listen(3333);
