fdf5120083ac4ad0fec3b61801750992dba91958
Evandro <evandro.guimaraes@pmweb.com.br>
2020-06-05 18:14:53 -0300

    include title and subtitle

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -8,7 +8,17 @@ const Home = () =>{
     return(
      <div id="page-home">
          <div className="content">
+             <header>
             <img src={logo} alt="Ecoleta"/>
+            </header>
+            <main>
+                <h1>
+                    Seu marketplace de coleta de resíduos.
+                </h1>
+                <p>
+                    Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+                </p>                
+            </main>
          </div>
      </div>
     )
