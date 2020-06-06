a0eac5350f8a99d9fcc8e30ba32926d130c61d4e
Evandro <evandro.guimaraes@pmweb.com.br>
2020-06-05 22:46:24 -0300

    implement map and pin for user

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import './styles.css';
 
@@ -66,6 +67,12 @@ const CreatePoint = () => {
                             Selecione o endereço no mapa
                         </span>
                     </legend>
+                    <Map center={[-30.048256,-51.2]} zoom={15}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[-30.048256,-51.2]} />
+                    </Map>
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
