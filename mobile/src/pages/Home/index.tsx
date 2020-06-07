a4b6b7771cb54a3aeb1f109a6ca2a5ac86b3588d
Evandro <evandro.guimaraes@pmweb.com.br>
2020-06-07 18:55:06 -0300

    early home page for app

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
 
 
 const Home = () => {
     return (
-    <View style={styles.container}>
+    <ImageBackground 
+      source={require('../../assets/home-background.png')} 
+      style={styles.container}
+      imageStyle={{width: 274, height: 368}}      
+    >
+      <View style={styles.main}>
         <Image source={require('../../assets/logo.png')} />
-    </View>)
+        <Text style={styles.title}>Seu marketplace  de coleta de resíduos</Text> 
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text> 
+      </View>
+        <View style={styles.footer}>
+
+        </View>
+    </ImageBackground>)
 };
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 32,
+      backgroundColor: '#f0f0f5'
     },
   
     main: {
